0b63609eceafd73dc15d85dd63d3279ecb99723d
Gendos <striker2011@gmail.com>
2023-09-19 18:19:10 +0300

    add generator

---
+++ generator/generator2.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// используется чтоб создать упорядоченную последовательность значений потенциально бесконечную
+func main() {
+	done := make(chan struct{})
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	ch := makeGenerator(done, &wg)
+
+	go func() {
+		defer wg.Done()
+		for v := range ch {
+			fmt.Println("value:", v)
+		}
+	}()
+
+	time.Sleep(time.Second)
+	close(done)
+	wg.Wait()
+}
+
+func makeGenerator(done <-chan struct{}, wg *sync.WaitGroup) <-chan int {
+	ch := make(chan int, 1)
+	var i = 0
+
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-done:
+				close(ch)
+				fmt.Println("done")
+				return
+			default:
+				time.Sleep(time.Millisecond * 250)
+				ch <- i
+				i++
+			}
+		}
+	}()
+
+	return ch
+}
