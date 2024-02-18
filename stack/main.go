d3139a4312683fff15ebf30958b7abdd52c800b1
Gendos <striker2011@gmail.com>
2024-02-18 18:04:06 +0200

    add stack description

--- stack/main.go
+++ stack/main.go
@@ -2,12 +2,14 @@ package main
 
 import "fmt"
 
+// стек на основе связного списка
+
 type item struct {
-	value int
-	next  *item
+	value int   // значение
+	next  *item // указатель на следующий элемент
 }
 
-type Stack struct {
+type Stack struct { // струкрута стека
 	head *item
 }
 
@@ -15,7 +17,7 @@ func NewStack() Stack {
 	return Stack{}
 }
 
-func (s *Stack) Push(value int) {
+func (s *Stack) Push(value int) { // создаем ноду, присваиваем следующее значение головы
 	s.head = &item{
 		value: value,
 		next:  s.head,
@@ -27,8 +29,8 @@ func (s *Stack) Pop() int {
 		return -1
 	}
 
-	value := s.head.value
-	s.head = s.head.next
+	value := s.head.value // запоминаем значение головы
+	s.head = s.head.next  // двигаем голову на следующий элемент
 	return value
 }
 
